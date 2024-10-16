03e714beb8f450e0fc00d7e96d6fc12281e97624
rdx40 <ivanjaison@gmail.com>
2024-10-16 12:11:34 +0530

    Basic Data Types

---
+++ learn/02-Variables Operators DataTypes/main.R
@@ -0,0 +1,82 @@
+#expression
+10 + 20
+
+#Hello World
+
+## 2ways of outpputing to cmd line
+
+"Hello World"
+
+print("Hello World")
+
+
+##variables
+a <- 10 ## = also works
+b <- 20
+
+a + b
+
+a <- a + 1
+
+a + b
+
+##arithmetic Operations
+
+10 + 20
+10 - 20
+10 / 20
+10 * 20
+10 ^ 20
+10 %% 3 
+
+(a + 10) / b + 2 * 8
+
+sqrt(16)
+exp(2)
+abs(-9)
+ceiling(10.27)
+floor(10.27)
+round(10.57)
+max(10,201,2102,323,32,2,1,4,6654)
+min(10,201,2102,323,32,2,1,4,6654)
+
+class(10.27)
+class(10)
+class(10L)
+class("10L")
+class(T)
+class(F)
+class(TRUE)
+class(FALSE)
+
+as.character(10.27)
+as.double("10.27") + 10
+
+as.logical(19)
+as.logical(0)
+
+text<- "Hello World"
+nchar(text)
+x <- paste("Hello", "World", "omar")
+
+cat("Hello" ,"World")
+
+long_text = "Hello
+this 
+is a
+PSA
+from 
+omar"
+
+print(long_text)
+cat(long_text)
+
+
+
+##logical and comparison operators
+T && F
+F && F
+T || F
+F || F
+
+(a < 5) || (a%%2 == 0) || (a>7)
