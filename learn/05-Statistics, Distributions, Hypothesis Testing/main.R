b5292be4141c3506142dec8b88e89b06cd1f3af5
rdx40 <ivanjaison@gmail.com>
2024-10-21 23:04:14 +0530

    updated

--- learn/05-Statistics, Distributions, Hypothesis Testing/main.R
+++ learn/05-Statistics, Distributions, Hypothesis Testing/main.R
@@ -38,3 +38,55 @@ cor(study_hours, blood_pressure)
 cov(study_hours, blood_pressure)
 
 
+##Prob Distributions (normal and binomial)
+
+#normal distribution
+
+#density function
+dnorm(3,0,5) #for a std deviation with mean 0 and sd=5 whats the chance of getting teh val 3 we get 6%
+dnorm(23,0,5) ##almsot 0 possibility ofgetting 23
+
+
+#cumulative density
+pnorm(3,0,5) #now asking what proprtion of teh data falls below 3 given a mean of 0 and sd of 5
+pnorm(1000,0,5)
+pnorm(-1000,0,5)
+pnorm(0,0,5)
+
+
+##what val corresponds to whatver percentile in data
+qnorm(0.95, 0, 5) 
+pnorm(8.224268,0,5)
+
+#genrates vals based on a distrib
+gen_vals <- rnorm(10000,0,5) 
+mean(gen_vals)
+sd(gen_vals)
+
+
+gen_vals <- rnorm(10,0,5) 
+mean(gen_vals)
+sd(gen_vals)
+
+
+#binomail fns
+dbinom(10, 100, 0.1) #how likely to get 10 successes when 100 trials done and prob is 10%
+
+dbinom(1, 100, 0.1)
+dbinom(9, 100, 0.1)
+
+
+
+pbinom(10,100,0.1)##how likely to get a max of successes given a experiment
+pbinom(100,100,0.1)
+pbinom(1,100,0.1)
+
+
+#hypothesis testing (is the mean of the sample significantly different from the hypothesised mean od the sample)
+
+gen_vals <- rnorm(1000,0,2)
+
+
+t.test(gen_vals, mu=0)#looking at this smaple it it possible to get a mean value of 0
+#any p val below 0.05 aint good
+
